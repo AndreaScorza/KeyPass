e66f9bd0e6b62050040772dd82e7b8940ed2fd93
Andre <atlantas95@gmail.com>
2021-09-13 21:53:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-from setuptools import setup, find_packages
+
+
+
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "KeyPass"
 
 setup(
-    name='KeyPass',
-    extras_require=dict(tests=['pytest']),
+    name=PACKAGE_NAME,
+    author="Bryce Boe",
+    author_email="",
+    python_requires="~=3.6",
+    description=(
+        "PRAW, an acronym for Python Reddit API Wrapper, is a python package that"
+        " allows for simple access to reddit's API."
+    ),
+
+    #package_data={"": ["LICENSE.txt"], PACKAGE_NAME: ["*.ini", "images/*.jpg"]},
+    #packages=find_packages(exclude=["tests", "tests.*", "tools", "tools.*"]),
     packages=find_packages(),
-    py_modules=['keyPass']
+    version=1,
 )
