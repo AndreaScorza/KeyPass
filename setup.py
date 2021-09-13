15c8d873a9921eb34a5f5e37f20c3de4d5852dfe
Andre <atlantas95@gmail.com>
2021-09-13 15:25:17 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='KeyPass',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='andrea',
+    author_email='atlantas95@gmail.com',
+    description='KeyPass'
+)
