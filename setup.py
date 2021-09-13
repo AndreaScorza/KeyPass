a6b8d55105796f1942fbe6a35499658f8a77a5fe
Andre <atlantas95@gmail.com>
2021-09-13 21:45:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='KeyPass',
     extras_require=dict(tests=['pytest']),
-    #package=find_packages(where='src'),
-    #package_dir={"":"src"},
-    packages=find_packages()
+    packages=find_packages(),
+    py_modules=['keyPass']
 )
